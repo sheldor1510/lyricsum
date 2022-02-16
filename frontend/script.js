a11ef7567db311c8fb8414523ec1045bbae716dd
Anshul Saha <anshulsaha15@gmail.com>
2022-02-16 15:19:15 +0530

    re-structured

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,45 @@
+const ls = window.localStorage;
 let suggestionFilled = null;
 let filled = false;
-let initialURL = 'http://localhost:5000';
-
-let paragraphs = 5;
-let paraLength = "short";
-let capitalization = "aa";
-let style = { 
-    selected: false, 
-    selections: [ "italics", "bold" ] 
-};
+let apiBaseURL = 'http://localhost:5000';
 let initialText = '';
 const paraLengths = { short: 50, medium: 100, long: 200 }
+let format = {
+    paragraphs: 7, 
+    paraLength: "short", 
+    capitalization: "aa", 
+    selections: []
+}
+
+if (ls.getItem('formatData') === null) {
+    let formatData = JSON.stringify(format)
+    ls.setItem('formatData', formatData);
+    console.log("added", formatData)
+} else {
+    format = JSON.parse(ls.getItem('formatData'));
+    console.log("saved version found", format)
+}
+
+const inflateFormatting = () => {
+    ls.setItem('formatData', JSON.stringify(format));
+    document.getElementById('para-count').innerHTML = format.paragraphs;
+    document.getElementById('range').value = format.paragraphs;
+    document.querySelector('input[value="' + format.paraLength + '"]').checked = true;
+    document.querySelector('input[value="' + format.capitalization + '"]').checked = true;
+    try {
+        if (format.selections.length == 0) {
+            document.querySelector('input[value="p"]').checked = false;
+            document.querySelector('input[value="bold"]').checked = false;
+            document.querySelector('input[value="italics"]').checked = false;
+        } else {
+            format.selections.forEach(selection => {
+                document.querySelector('input[value="' + selection + '"]').checked = true;
+            });
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 function autocomplete(inp) {
     var currentFocus;
@@ -26,20 +55,22 @@ function autocomplete(inp) {
             a.setAttribute("id", this.id + "autocomplete-list");
             a.setAttribute("class", "autocomplete-items");
             this.parentNode.appendChild(a);
-            fetch(`${initialURL}/suggest?query=${val}`)
+            fetch(`${apiBaseURL}/suggest?query=${val}`)
             .then(async (res) => {
                 const resp = await res.json();
                 //console.log(resp.artists);
                 let arr = resp.artists.slice(0, 5);
+                document.getElementById('search-img').style.display = 'none'
                 for (i = 0; i < arr.length; i++) {
                     b = document.createElement("DIV");
                     b.innerHTML += arr[i].substr(0, val.length) + arr[i].substr(val.length);
                     b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
                     b.addEventListener("click", function(e) {
+                        document.getElementById('search-img').style.display = 'block'
                         inp.value = this.getElementsByTagName("input")[0].value;
-                        document.getElementById('input-wrapper').innerHTML += `<div class="tag">${inp.value}<i class="fa fa-times" aria-hidden="true" style="margin-left: 5px;" id="cross-icon"></i></div>`
-                        document.getElementsByTagName("input")[0].style.display = 'none'
-                        document.getElementsByTagName("input")[0].value = inp.value 
+                        document.getElementById('input-area').innerHTML += `<div class="tag">${inp.value}<img style="margin-left: 1vw;" id="cross-icon" src="assets/cross.svg"></img></div>`
+                        document.getElementById("artist-input").style.display = 'none'
+                        document.getElementById("artist-input").value = inp.value
                         suggestionFilled = inp.value;
                         closeAllLists();
                     });
@@ -90,11 +121,13 @@ function autocomplete(inp) {
     });
 }
 
+
 const addStyling = (paramString) => {
-    if (style.selected) {
+    let selections = format.selections;
+    if (selections.length > 1) {
         let randomBool = Math.floor(Math.random() * 3)
         if (randomBool === 1) {
-            if (style.selections.length > 1) {
+            if (selections.length > 2) {
                 let anotherRandomBool = Math.floor(Math.random() * 2)
                 if (anotherRandomBool == 1) {
                     paramString = '<i>' + paramString + '</i>'
@@ -102,9 +135,9 @@ const addStyling = (paramString) => {
                     paramString = '<b>' + paramString + '</b>'
                 }
             } else {
-                if (style.selections[0] === "italics") {
+                if (selections[1] === "italics") {
                     paramString = '<i>' + paramString + '</i>'
-                } else if (style.selections[0] === "bold") {
+                } else if (selections[1] === "bold") {
                     paramString = '<b>' + paramString + '</b>'
                 }
             }
@@ -122,6 +155,9 @@ const isLetter = (str) => {
 }   
 
 const formatText = () => {
+    let paraLength = format.paraLength;
+    let paragraphs = format.paragraphs;
+    let capitalization = format.capitalization;
     initialText = document.getElementById('lyrics-text').innerText
     let localInitialText = initialText
     let htmlToAppend = ''
@@ -184,37 +220,109 @@ const formatText = () => {
     document.getElementById('lyrics-text').innerHTML = htmlToAppend
 }
 
+const inflateSong = (data) => {
+    document.getElementById('lyrics-text').innerText = data.lyrics
+    formatText();
+    document.getElementById('song-img').src = data.currentSong.song_art_image_thumbnail_url
+    let songTitle = data.currentSong.title
+    let songArtist = data.searchQuery
+    if (songArtist == "random") {
+        songArtist = data.currentSong.artist_names
+    }
+    if (songTitle.length > 24) {
+        songTitle = songTitle.slice(0, 24) + "..."
+    }
+    if (songArtist.length > 24) {
+        songArtist = songArtist.slice(0, 24) + "..."
+    }
+    document.getElementById('song-title').innerText = songTitle
+    document.getElementById('song-artist').innerText = songArtist
+    document.getElementById('song-link').href = data.currentSong.url
+}
+
+const logSong = (data) => {
+    let log = {}
+    log.song = data.currentSong.title
+    log.artist = data.currentSong.artist_names
+    log.paragraphs = format.paragraphs
+    log.para_length = format.paraLength
+    log.capitalization = format.capitalization
+    let selectionString = ''
+    format.selections.forEach(selection => {
+        selectionString += selection + ','
+    })
+    log.selections = selectionString
+    log.logged_at = new Date().toLocaleString()
+    console.log(log)
+    fetch(apiBaseURL + '/log', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(log)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+const generateInputSong = () => {
+    document.getElementById('generate-button').disabled = true;
+    document.getElementById('lyrics-text').innerHTML = '<iframe src="https://cdn.discordapp.com/attachments/766636913343463454/943389634052894760/loader-ly.gif" scrolling="no" id="loader"></iframe>';
+    fetch(apiBaseURL + '/generate?query=' + document.getElementById('artist-input').value.trim())
+    .then(async (response) => {
+        const resp = await response.json()
+        console.timeEnd('GenerationTime');
+        inflateSong(resp);
+        document.getElementById('generate-button').disabled = false;
+        document.body.style.overflowY = 'initial';
+        logSong(resp);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+const generateRandomSong = () => {
+    document.getElementById('generate-button').disabled = true;
+    document.getElementById('lyrics-text').innerHTML = '<iframe src="https://cdn.discordapp.com/attachments/766636913343463454/943389634052894760/loader-ly.gif" scrolling="no" id="loader"></iframe>';
+    fetch(apiBaseURL + '/random')
+    .then(async (response) => {
+        const resp = await response.json()
+        console.timeEnd('GenerationTime');
+        inflateSong(resp);
+        document.getElementById('generate-button').disabled = false;
+        document.body.style.overflowY = 'initial';
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
 window.onload = () => {
-    autocomplete(document.getElementById("myInput"));
-    document.getElementById('check').addEventListener('click', () => {
-        console.time('checkTime');
-        if (document.getElementById('myInput').value.trim() == suggestionFilled) {
-            filled = true;
+    var slider = document.getElementById("range");
+    var output = document.getElementsByClassName('para-count')[0];
+    output.innerHTML = slider.value;
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+        format.paragraphs = this.value;
+        inflateFormatting();
+        formatText();
+    }
+    document.body.style.overflowY = 'hidden';
+    autocomplete(document.getElementById("artist-input"));
+    document.getElementById('generate-button').addEventListener('click', () => {
+        console.time('GenerationTime');
+        document.body.style.overflowY = 'hidden';
+        if (document.getElementById('artist-input').value.trim() == suggestionFilled) {
+            generateInputSong();
         } else {
-            filled = false;
-        }
-        if (filled) {
-            fetch(initialURL + '/generate?query=' + document.getElementById('myInput').value.trim())
-            .then(async (response) => {
-                const resp = await response.json()
-                console.log(resp);
-                document.getElementById('lyrics-text').innerText = resp.lyrics
-                console.timeEnd('checkTime');
-                formatText();
-                document.getElementById('song-img').src = resp.currentSong.song_art_image_thumbnail_url
-                document.getElementById('song-title').innerText = resp.currentSong.title
-                document.getElementById('song-link').href = resp.currentSong.url
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        } else {
-            alert('Please pick a suggestion');
+            generateRandomSong();
         }
     })
-    document.getElementById('copy').addEventListener('click', () => {
+    document.getElementById('copy-button').addEventListener('click', () => {
         let copyText = ''
-        if (style.selected) {
+        if (format.selections.length > 0) {
             copyText = document.getElementById('lyrics-text').innerHTML
         } else {
             copyText = document.getElementById('lyrics-text').innerText
@@ -225,13 +333,58 @@ window.onload = () => {
         copy.select();
         document.execCommand("copy");
         document.body.removeChild(copy);
+        const notyf = new Notyf({
+            duration: 1500,
+            position: { x: "right", y: "top" },
+            closeBtn: {
+                text: 'X',
+            }
+        });
+        notyf.success('Copied to clipboard!');
     })
     window.addEventListener('click', (e) => {
         if (e.target.id === "cross-icon") {
-            document.getElementById('myInput').style.display = 'block'
-            document.getElementById('myInput').value = ''
+            document.getElementById('artist-input').style.display = 'block'
+            document.getElementById('artist-input').value = ''
             document.getElementsByClassName('tag')[0].remove()
-            autocomplete(document.getElementById("myInput"));
+            autocomplete(document.getElementById("artist-input"));
+        }
+        if (["short", "medium", "long"].includes(e.target.value)) {
+            format.paraLength = e.target.value;
+            inflateFormatting();
+            formatText();
+        }
+        if (["aa", "AA", "Aa"].includes(e.target.value)) {
+            format.capitalization = e.target.value;
+            inflateFormatting();
+            formatText();
+        }
+        if (["p", "bold", "italics"].includes(e.target.value)) {
+            if (e.target.value === "p") {
+                if (format.selections.includes(e.target.value)) {
+                    format.selections = [];
+                } else {
+                    format.selections = ['p'];
+                }
+            } else {
+                if (format.selections.includes(e.target.value)) {
+                    if (format.selections.length > 2) {
+                        format.selections = format.selections.filter(item => item !== e.target.value)
+                    } else {
+                        format.selections = ["p"];
+                    }
+                } else {
+                    if (format.selections.length == 2) {
+                        format.selections.push(e.target.value);
+                    } else {
+                        format.selections = ["p", e.target.value];
+                    }
+                }
+            }
+            inflateFormatting();
+            formatText();
         }
     })
+    inflateFormatting();
+    generateRandomSong();
 }
